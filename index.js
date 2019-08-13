6d14689e622e55bcbfb8c2152201f413ceabdd66
Andre <flpandresilva@gmail.com>
2019-08-13 09:29:46 -0300

    Merging test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const { prisma } = require("./generated/prisma-client")
-const { GraphQLServer } = require("graphql-yoga")
-const resolvers = require("./src/resolvers/resolvers")
+const { prisma } = require("./generated/prisma-client");
+const { GraphQLServer } = require("graphql-yoga");
+const resolvers = require("./src/resolvers/resolvers");
 
 const options = {
   port: 4000,
   endpoint: "/graphql",
   subscriptions: "/subscriptions",
   playground: "/playground"
-}
+};
 
 const server = new GraphQLServer({
   typeDefs: "./schema.graphql",
@@ -15,7 +15,8 @@ const server = new GraphQLServer({
   context: {
     prisma
   }
-})
+});
+
 server.start(options, () =>
   console.log("Server is running on http://localhost:4000/playground")
-)
+);
